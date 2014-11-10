2b6dfcc45b59289b1318ee5050cd64f5f8e6dc6d
Peter Henry <phenryextra@zoho.com>
2014-11-09 20:05:50 -0600

    sample member data

--- app/views.py
+++ app/views.py
@@ -5,6 +5,30 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'marseilles'}
+    members = [
+                {
+                    {'fbname': 'Joshua Manu'}
+                    {'stname': 'redsurfable'}
+                    {'dbid': '0'}
+                    {'lastseen': '7 months ago'}
+                },
+                {
+                    {'fbname': 'Christopher Poenaru'}
+                    {'stname': 'Swambulance'}
+                    {'dbid': '0'}
+                    {'lastseen': '7 months ago'}
+                }
+                {
+                    {'fbname': 'Sam Cho'}
+                    {'stname': 'Mikasa'}
+                    {'dbid': '0'}
+                    {'lastseen': '7 months ago'}
+                }
+            ]
+
+
+
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           members=members)
